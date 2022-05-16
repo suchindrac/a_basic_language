3bc505e2da820880b8c990bcf9fcb1920ba7bb36
Suchindra Chandrahas <suchi.c@outlook.com>
2022-05-16 11:41:53 +0530

    Modified block parsing logic

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+echo "Output of first script:" > output.txt
 python3 interpreter.py -s test/script.bl | tee output.txt
+echo "Output of second script:" >> output.txt
+python3 interpreter.py -s test/blk.bl | tee -a output.txt
