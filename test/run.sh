2b84231de136452eb31719dc632119a1cdd93ff5
Suchindra Chandrahas <suchi.c@outlook.com>
2022-05-16 21:37:52 +0530

    Added test cases

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,7 @@
 echo "Output of script 1:" > output.txt
 python3 ../interpreter.py -s script.bl | tee output.txt
+echo "Output of script 1 from main block:" >> output.txt
+python3 ../interpreter.py -s scriptmain.bl | tee -a output.txt
 echo "Output of script 2:" >> output.txt
 python3 ../interpreter.py -s blk.bl | tee -a output.txt
 echo "Output of script 3:" >> output.txt
